a76e056d224233c1fc0af7fc63561253004b1fcb
Max <maxvetrenko2241@gmail.com>
2014-06-28 17:29:31 +0400

    create working test

--- src/test/java/com/google/checkstyle/test/base/InputSimpleTest.java
+++ src/test/java/com/google/checkstyle/test/base/InputSimpleTest.java
@@ -1,9 +1,5 @@
 package com.google.checkstyle.test.base;
 
-
-import static org.junit.Assert.*;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,12 +8,8 @@ import java.util.Properties;
 
 import org.junit.Test;
 
-import com.puppycrawl.tools.checkstyle.Checker;
 import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
-import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
-import com.puppycrawl.tools.checkstyle.DefaultLogger;
 import com.puppycrawl.tools.checkstyle.PropertiesExpander;
-import com.puppycrawl.tools.checkstyle.api.AuditListener;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
 
@@ -46,65 +38,54 @@ public class InputSimpleTest extends BaseCheckTestSupport{
         System.out.println("Root is set to \"" + ROOT.getAbsolutePath() + "\".");
         
     	Configuration conf = getConfigurationFromXML("checkstyle_google_style.xml", System.getProperties());
-    	Configuration defConfig = conf.getChildren()[1].getChildren()[5];
     	
-    	List files = new ArrayList();
+    	List<File> files = new ArrayList();
         listFiles(files, ROOT, "java");
         System.out.println("Found " + files.size() + " Java source files.");
 
-//    	ByteArrayOutputStream sos = new ByteArrayOutputStream();
-//    	AuditListener listener = new DefaultLogger(sos, false);
-    	
-//        Checker checker = createChecker(conf);
-        
-//        int errors = checker.process(files);
-//        System.out.println("Found " + errors + " check style errors.");
-//        System.out.println(sos.toString());
-//        assertTrue(errors + " check style errors found. " + sos.toString(), errors == 0);
-        
         final String[] expected = {
-                "4:1: '{' should be on the previous line.",
-                "13:5: Variable access definition in wrong order.",
-                "18:5: Variable access definition in wrong order.",
-                "21:5: Variable access definition in wrong order.",
-                "27:5: Static variable definition in wrong order.",
-                "27:5: Variable access definition in wrong order.",
-                "34:9: Variable access definition in wrong order.",
-                "45:9: Static variable definition in wrong order.",
-                "45:9: Variable access definition in wrong order.",
-                "54:5: Constructor definition in wrong order.",
-                "80:5: Instance variable definition in wrong order.",
-
-                "92:9: Variable access definition in wrong order.",
-                "100:9: Static variable definition in wrong order.",
-                "100:9: Variable access definition in wrong order.",
-                "106:5: Variable access definition in wrong order.",
-                "111:5: Variable access definition in wrong order.",
-                "116:5: Variable access definition in wrong order.",
-                "119:5: Variable access definition in wrong order.",
-                "125:5: Static variable definition in wrong order.",
-                "125:5: Variable access definition in wrong order.",
-                "132:9: Variable access definition in wrong order.",
-                "143:9: Static variable definition in wrong order.",
-                "143:9: Variable access definition in wrong order.",
-                "152:5: Constructor definition in wrong order.",
-                "178:5: Instance variable definition in wrong order.",
-            };
-            String path = files.get(1).toString();
-			verify(defConfig, path, expected);
+        		"14:13: Fall through from previous branch of the switch statement.",
+        		"38:13: Fall through from previous branch of the switch statement.",
+        		"53:13: Fall through from previous branch of the switch statement.",
+        		"70:13: Fall through from previous branch of the switch statement.",
+        		"87:13: Fall through from previous branch of the switch statement.",
+        		"105:13: Fall through from previous branch of the switch statement.",
+        		"123:13: Fall through from previous branch of the switch statement.",
+        		"369:11: Fall through from previous branch of the switch statement.",
+        		"372:11: Fall through from previous branch of the switch statement.",
+        		"374:40: Fall through from previous branch of the switch statement.",
+        		};
+        	String rootPath = ROOT.getAbsolutePath().substring(0, ROOT.getAbsolutePath().lastIndexOf("src"));
+            String path = rootPath + getFilePath(files, "InputFallThrough");
+			verify(getCheckConfig(conf, "FallThrough"), path, expected);
         
     }
 
-	private Checker createChecker(final Configuration conf, final AuditListener listener)
-			throws CheckstyleException {
-		final Checker checker = new Checker();
-        checker.setModuleClassLoader(Checker.class.getClassLoader());
-        checker.configure(conf);
-        checker.addListener(listener);
-		return checker;
-	}
+    private Configuration getCheckConfig(Configuration aConfig, String aCheckName) {
+    	for(Configuration config : aConfig.getChildren()) {
+    	    if("TreeWalker".equals(config.getName())) {
+    	    	for(Configuration checkConfig : config.getChildren()) {
+    	    		if (aCheckName.equals(checkConfig.getName())) {
+    	    			return checkConfig;
+    	    		}
+    	    	}
+    	    } else if (aCheckName.equals(config.getName())) {
+    	    	return config;
+    	    }
+    	}
+        return null;    
+    }
     
-    private static void listFiles(final List files, final File folder, final String extension) {
+    private String getFilePath (List<File> aFiles, String aFileName) {
+    	for (File file : aFiles) {
+    		if (file.toString().contains(aFileName)) {
+    			return file.toString();
+    		}
+    	}
+    	return null;
+    }
+    
+    private static void listFiles(final List<File> files, final File folder, final String extension) {
         if (folder.canRead()) {
             if (folder.isDirectory()) {
                 for (final File f : folder.listFiles()) {
