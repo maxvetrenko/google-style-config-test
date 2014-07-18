cf54c532c5fb52896f2f7fc71d61fb522ab066d3
Max <maxvetrenko2241@gmail.com>
2014-07-18 20:32:41 +0400

    Added AvoidEscapedUnicodeCharactersCheck

---
+++ src/main/java/com/google/checkstyle/test/chapter2filebasic/rule233nonascii/AvoidEscapedUnicodeCharactersInput.java
@@ -0,0 +1,38 @@
+package com.google.checkstyle.test.chapter2filebasic.rule233nonascii;
+
+public class AvoidEscapedUnicodeCharactersInput {
+	
+	private String unitAbbrev2 = "\u03bcs";
+
+	private String unitAbbrev3 = "\u03bcs"; //Greek letter mu
+
+	private String unitAbbrev4 =
+			"\u03bcs"; //Greek letter mu
+
+	public Object fooString()
+	{
+		String unitAbbrev = "μs";
+		String unitAbbrev2 = "\u03bcs";
+		String unitAbbrev3 = "\u03bcs"; // Greek letter mu, "s"
+		String fakeUnicode = "asd\tsasd";
+		String fakeUnicode2 = "\\u23\\u123i\\u";
+		String content = "";
+		return "\ufeff" + content ; // byte order mark
+	}
+
+	public Object fooChar()
+	{
+		char unitAbbrev2 = '\u03bc';
+		char unitAbbrev3 = '\u03bc'; // Greek letter mu, "s"
+		String content = "";
+		return '\ufeff' + content; // byte order mark
+	}
+
+	public void multiplyString()
+	{
+		String unitAbbrev2 = "asd\u03bcsasd";
+		String unitAbbrev3 = "aBc\u03bcssdf\u03bc"; /* Greek letter mu, "s"*/
+		String unitAbbrev4 = "\u03bcaBc\u03bcssdf\u03bc";
+		String allCharactersEscaped = "\u03bc\u03bc";
+	}
+}
